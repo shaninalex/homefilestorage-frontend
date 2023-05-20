4e834f1d322de67a21f35b6dc0dc936a58b736f2
shaninalex <shanin.x@gmail.com>
2023-05-20 13:46:32 +0300

    files list

--- src/app/home/pages/file-list/file-list.component.ts
+++ src/app/home/pages/file-list/file-list.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { FilesState } from '../../store/files/files.reducer';
+import { StorageFile } from '../../models/files.models';
+import { selectFiles } from '../../store/files/files.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-file-list',
-  templateUrl: './file-list.component.html'
+    selector: 'app-file-list',
+    templateUrl: './file-list.component.html'
 })
 export class FileListComponent {
+
+    files: Observable<StorageFile[]>;
+
+    constructor(private store: Store<FilesState>) {
+        this.files = this.store.pipe(select(selectFiles));
+    }
 
 }
